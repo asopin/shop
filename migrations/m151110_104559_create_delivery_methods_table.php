db7d7fcfb79d7c90216cb0e5ac7ed69a5e634828
asopin <alexey.sopin@gmail.com>
2015-11-10 14:24:30 +0200

    migrations templates created

---
+++ migrations/m151110_104559_create_delivery_methods_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m151110_104559_create_delivery_methods_table extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m151110_104559_create_delivery_methods_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
---
+++ migrations/m151110_105002_create_orders_history_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m151110_105002_create_orders_history_table extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m151110_105002_create_orders_history_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
